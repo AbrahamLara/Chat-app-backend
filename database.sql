18ef79917e0b76ff582bac5a915e69f5df2a5617
Abraham Lara Granados <abrahamlara19@gmail.com>
2020-11-14 19:28:13 -0500

    fix(database script): Uncommented line creating database and role in sql script

--- database.sql
+++ database.sql
@@ -1,4 +1,4 @@
 DROP DATABASE IF EXISTS chatapp;
 DROP ROLE IF EXISTS chatapp;
--- Comment the next line so that you can drop the database and role.
---CREATE DATABASE chatapp; CREATE ROLE chatapp WITH LOGIN PASSWORD 'chatapp';
+-- Comment the next line so that you only drop the database and role that was created for this project.
+CREATE DATABASE chatapp; CREATE ROLE chatapp WITH LOGIN PASSWORD 'chatapp';
